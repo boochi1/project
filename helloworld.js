5f4c718acac8e831f045cf78bdf2960e4b1e2fd5
boochi1 <gbenga.ezemadu@outlook.com>
2020-03-25 14:29:56 +0100

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,19 @@
+var name = "Chimbuchi Ezemadu";
+console.log("My name is: " + name);
+
+var course = ["Frontend ", "Backend ", "Design ", "Mobile ", "Coding"];
+console.log("The courses I do are: " + course)
+
+ const data = [];
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+
+ }
+
+ console.log(data);
